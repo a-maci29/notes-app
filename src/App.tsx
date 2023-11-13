23113ce93acc7df55455b1480291c3a0460a2f3e
alexa <alexamacias192@gmail.com>
2023-11-13 00:16:06 -0500

    Removes boilerplate elements and adds basic elements for UI

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="AppContainer">
+      <form className="note-form">
+        <input placeholder="Title" required />
+        <textarea placeholder="Content" rows={10}/>
+
+        <button type="submit">Add note</button>
+
+      </form>
+
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button>X</button>
+          </div>
+
+          <h2>Note Title</h2>
+          <p>Note Content</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
