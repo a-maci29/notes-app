6f8b2596af081566df96d9bae7d9c62496c280ab
alexa <alexamacias192@gmail.com>
2023-11-14 23:48:48 -0500

    Adds MUI and some test elements

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import Button, { ButtonTypeMap } from '@mui/material/Button';
+import { ExtendButtonBase } from '@mui/material';
 
 export interface Props {
   title: string
   color?: string
 }
 
+export interface Button {
+  type: "submit"
+}
+
 const Header = (props: Props) => {
   return(
     <header>
       <h1>Hello</h1>
+      <Button variant="outlined">Soy un Button</Button>
     </header>
   )
 }
 
-export default Header
+export default Header;
